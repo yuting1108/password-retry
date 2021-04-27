ef5da71f29c5296f406c87155419dbc28f286cf1
yuting1108 <sweet5845187@gmail.com>
2021-04-27 11:53:49 +0800

    basic test

---
+++ pwd.py
@@ -0,0 +1,18 @@
+#password = 'a123456'
+#請使用者重複輸入密碼
+#最多三次
+#正確的話，印出'登入成功'
+#錯誤的話，印出'密碼錯誤!還有_次機會!'
+password = 'a123456'
+x = 3
+while True:
+	pwd = input('請輸入密碼:')
+	if pwd == password:
+		print('登入成功!')
+		break
+	else:
+		x = x - 1
+		print('密碼錯誤!還有', x, '次機會!')
+		if x == 0:
+			break
+			
